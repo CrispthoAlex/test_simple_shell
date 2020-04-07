00c37ddeaad38913ea724e1a08302367d1f959c8
crispthoalex <alex877@gmail.com>
2020-04-07 17:09:43 +0000

    add mini-shell fixed

--- prompt.c
+++ prompt.c
@@ -15,9 +15,9 @@ int main(void)
 	size_t len = 0;
 	ssize_t read;/*chars read by getline() */
 	char *tokens = NULL;/* containt each string typing */
-	char *limstr[] = {" ","\t", '\0'};/* string delimited */
+	/*char *limstr[] = {" ", "\t", "\0", "\\-r"}; string delimited */
 	char *argstr[100];/* arguments input*/
-	int i;
+	int i = 0;
 	int exe_child;/*value return by execve*/
 	int status;/*status return*/
 
@@ -26,22 +26,23 @@ int main(void)
 	/*read = getline(&line, &len, fp);*/
 	while (1)
 	{
-		printf("ygi$> ");/*prompt */
+		printf("#cisfun$> ");/*prompt */
 
 		/*// READING PHASE \\ */
 		read = getline(&line, &len, stdin);
 		if (read == -1)
 			break;
-		printf("%s", line);
 
-		tokens = strtok(line, *limstr);
+		tokens = strtok(line, " \t\n\r");
 
-		for (i = 0; tokens != NULL; i++)/*token each string*/
+		i = 0;
+		while (tokens != NULL)/*token each string*/
 		{
 			argstr[i] = tokens;
-			tokens = strtok(NULL, *limstr);
-			printf("argstr[%d] = %s\n", i, argstr[i]);
-			printf("lenth string read %d\n", (unsigned int)len);
+			if(!strcmp("exit", argstr[0]))
+				exit(1);
+			tokens = strtok(NULL, " \t\n\r");
+			i++;
 		}
 		argstr[i] = NULL;
 
@@ -49,11 +50,15 @@ int main(void)
 			/*-----------------*/
 
 		/*// EXECUTE PHASE \\ */
+
 		exe_child = fork();
 		if (exe_child == 0)
 		{
-			if ((execve(argstr[0], argstr, NULL)) == -1)
+			if (execve(argstr[0], argstr, NULL) == -1)
+			{
 				perror("Do not execute the command");
+				exit(1);
+			}
 		}
 		else
 			wait(&status);
