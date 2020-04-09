8360f7da2c957ffa7eb792cc26400f79447ac43e
crispthoalex <alex877@gmail.com>
2020-04-09 20:16:19 +0000

    update prompt.c

--- prompt.c
+++ prompt.c
@@ -1,29 +1,25 @@
+#include "headershell.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <sys/wait.h>
+
+
 /**
- * main - read line
+ * main - Read, Search and execute the input
  *
  * Return: Always 0 success
  */
-int main(void)
+int main(int argc, char *argv[], char **env)
 {
+	(void)argc;
+	(void)argv;
 	char *line = NULL;
+	char **input; /* command typing by user */
+	char **binpath; /* pointer to PATH*/
+	int i, j;
 	size_t len = 0;
 	ssize_t read;/*chars read by getline() */
-	char *tokens = NULL;/* containt each string typing */
-	/*char *limstr[] = {" ", "\t", "\0", "\\-r"}; string delimited */
-	char *argstr[100];/* arguments input*/
-	int i = 0;
-	int exe_child;/*value return by execve*/
-	int status;/*status return*/
+	int exe_child; /*value return by execve*/
+	int status; /*status return*/
 
-
-	/*number of characters written into the buffer*/
-	/*read = getline(&line, &len, fp);*/
 	while (1)
 	{
 		printf("#cisfun$> ");/*prompt */
@@ -33,35 +29,34 @@ int main(void)
 		if (read == -1)
 			break;
 
-		tokens = strtok(line, " \t\n\r");
+		input = get_input(line); /*receive the command type by the user */
+		printf("VERIFICANDO");
 
-		i = 0;
-		while (tokens != NULL)/*token each string*/
-		{
-			argstr[i] = tokens;
-			if(!strcmp("exit", argstr[0]))
-				exit(1);
-			tokens = strtok(NULL, " \t\n\r");
-			i++;
-		}
-		argstr[i] = NULL;
+		binpath = get_path(env);
+
+		i = 0, j = 0;
+		for (; input[i] != '\0'; i++)
+			printf("%s", input[i]);
+
+		for (; binpath[j] != '\0'; j++)
+                        printf("%s", binpath[j]);
 
 		/*// SEARCHING PATH PHASE \\ */
 			/*-----------------*/
+		exe_child = fork();
+                if (exe_child == 0)
+                {
+                        if (execve(input[0], input, NULL) == -1)
+                        {
+                                perror("Command not executed. Try again");
+                                exit(1);
+                        }
+                }
+                else
+			wait(&status);
 
 		/*// EXECUTE PHASE \\ */
 
-		exe_child = fork();
-		if (exe_child == 0)
-		{
-			if (execve(argstr[0], argstr, NULL) == -1)
-			{
-				perror("Do not execute the command");
-				exit(1);
-			}
-		}
-		else
-			wait(&status);
 	}
 	free(line);
 	return (0);
